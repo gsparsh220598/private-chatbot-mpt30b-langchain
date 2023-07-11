656050e40fc74a640be7ff77f3e4d74a53c13aee
Sparsh Gupta <gsparsh220598@gmail.com>
2023-07-11 15:17:43 +0530

    added support for openai llms

--- constants.py
+++ constants.py
@@ -3,6 +3,14 @@ import os
 from chromadb.config import Settings
 from dotenv import load_dotenv
 
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    AIMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain import PromptTemplate
+
 load_dotenv()
 
 # Define the folder for storing database on disk and load
@@ -15,3 +23,39 @@ CHROMA_SETTINGS = Settings(
     persist_directory=PERSIST_DIRECTORY,
     anonymized_telemetry=False,
 )
+
+template = """
+You are a highly intelligent and helpful research assistant specializing in AI and its applications. 
+You always answer truthfully and do not make up answers.
+Your job is to research relevent information from context. And answer the questions like an expert.
+If you do not find the an appropriate answer in context, just say I don't know.
+
+{context}
+"""
+system_message_prompt = SystemMessagePromptTemplate(
+    prompt=PromptTemplate(
+        input_variables=["context"],
+        output_parser=None,
+        partial_variables={},
+        template=template,
+        template_format="f-string",
+        validate_template=True,
+    ),
+    additional_kwargs={},
+)
+human_message_prompt = HumanMessagePromptTemplate(
+    prompt=PromptTemplate(
+        input_variables=["question"],
+        output_parser=None,
+        partial_variables={},
+        template="{question}",
+        template_format="f-string",
+        validate_template=True,
+    ),
+    additional_kwargs={},
+)
+PROMPT = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+
+CHAIN_TYPE = "stuff"
+CHAT_MODEL = "gpt-3.5-turbo-0613"
+COND_MODEL = "text-davinci-002"
--- question_answer_docs.py
+++ question_answer_docs.py
@@ -4,34 +4,66 @@ import time
 
 from dotenv import load_dotenv
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-from langchain.chains import RetrievalQA
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.llms import CTransformers
+from langchain.chains import RetrievalQA, ConversationalRetrievalChain
+from langchain.embeddings import HuggingFaceEmbeddings, OpenAIEmbeddings
+from langchain.retrievers.document_compressors import (
+    LLMChainExtractor,
+    LLMChainFilter,
+    EmbeddingsFilter,
+)
+from langchain.memory import ConversationBufferMemory
+from langchain.llms import CTransformers, OpenAI
+from langchain.chat_models import ChatOpenAI
 from langchain.vectorstores import Chroma
 
-from constants import CHROMA_SETTINGS
+from typing import Any, Dict
+from IPython.display import Markdown, HTML, display
+
+import openai
+
+
+from constants import *
 
 load_dotenv()
 
 embeddings_model_name = os.environ.get("EMBEDDINGS_MODEL_NAME")
 persist_directory = os.environ.get("PERSIST_DIRECTORY")
 model_path = os.environ.get("MODEL_PATH")
-target_source_chunks = int(os.environ.get("TARGET_SOURCE_CHUNKS", 4))
+target_source_chunks = int(os.environ.get("TARGET_SOURCE_CHUNKS", 10))
 
 
-def main():
+# TODO: cite where the soln has been taken from
+class AnswerConversationBufferMemory(ConversationBufferMemory):
+    def save_context(self, inputs: Dict[str, Any], outputs: Dict[str, str]) -> None:
+        return super(AnswerConversationBufferMemory, self).save_context(
+            inputs, {"response": outputs["answer"]}
+        )
+
+
+def main(oss=False):
     # Prepare the retriever
-    embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
+    if oss:
+        embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
+    else:
+        embeddings = OpenAIEmbeddings()
     db = Chroma(
         persist_directory=persist_directory,
         embedding_function=embeddings,
-        client_settings=CHROMA_SETTINGS,
+        # client_settings=CHROMA_SETTINGS,
     )
     retriever = db.as_retriever(search_kwargs={"k": target_source_chunks})
-    qa = RetrievalQA.from_chain_type(
-        llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True
+    memory = AnswerConversationBufferMemory(
+        memory_key="chat_history", return_messages=True
     )
-
+    qa = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=retriever,
+        memory=memory,
+        # condense_question_llm=cond_llm,
+        chain_type=CHAIN_TYPE,
+        return_source_documents=True,
+    )
+    qa.combine_docs_chain.llm_chain.prompt = PROMPT
     # Interactive questions and answers over your docs
     while True:
         query = input("\nEnter a question: ")
@@ -43,44 +75,49 @@ def main():
         # Get the answer from the chain
         try:
             print("Thinking... Please note that this can take a few minutes.")
-            # start = time.time()
-            res = qa(query)
-            answer, docs = res["result"], res["source_documents"]
-            # end = time.time()
+            start = time.time()
+            res = qa({"question": query})
+            answer, docs = res["answer"], res["source_documents"]
+            end = time.time()
 
-            # Print the result
-            print("\n\n> Question:")
-            print(query)
-            # print(f"\n> Answer (took {round(end - start, 2)} s.):")
-            print(answer)
+            # # Print the result
+            # print("\n\n> Question:")
+            # print(query)
+            print(f"\n> Answer (took {round(end - start, 2)} s.):")
+            # print(display(Markdown(answer)))
 
-            # Print the relevant sources used for the answer
-            for document in docs:
-                print("\n> " + document.metadata["source"] + ":")
-                print(document.page_content)
+            # # Print the relevant sources used for the answer
+            # for document in docs:
+            #     print("\n> " + document.metadata["source"] + ":")
+            #     print(document.page_content)
         except Exception as e:
             print(str(e))
             raise
 
 
-def load_model():
+def load_model(oss=False):
     try:
         # check if the model is already downloaded
-        if os.path.exists(model_path):
-            print("Loading model...")
-            global llm
-            # initialize llm
+        print("Loading model...")
+        global llm, cond_llm
+        # initialize llm
+        if oss:
             llm = CTransformers(
                 model=os.path.abspath(model_path),
                 model_type="mpt",
                 callbacks=[StreamingStdOutCallbackHandler()],
-                config={"temperature": 0.1, "stop": ["<|im_end|>", "|<"]},
+                config={"temperature": 0.1},
             )
-            return True
         else:
-            raise ValueError(
-                "Model not found. Please run `poetry run python download_model.py` to download the model."
+            llm = ChatOpenAI(
+                model_name=CHAT_MODEL,
+                temperature=0.1,
+                streaming=True,
+                callbacks=[StreamingStdOutCallbackHandler()],
             )
+            # cond_llm = OpenAI(model=COND_MODEL, temperature=0.1)
+        return True
+
     except Exception as e:
         print(str(e))
         raise
@@ -88,5 +125,6 @@ def load_model():
 
 if __name__ == "__main__":
     # load model if it has already been downloaded. If not prompt the user to download it.
-    load_model()
-    main()
+    oss = bool(input("Do you want to use open source stuff? (y/n): ") == "y")
+    load_model(oss)
+    main(oss)
