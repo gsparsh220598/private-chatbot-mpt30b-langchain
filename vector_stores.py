d4e873fa59d2bff144a072f6000dbca0e8bdfd58
Sparsh Gupta <gsparsh220598@gmail.com>
2023-07-13 22:30:34 +0530

    created separate functions for every vs

--- vector_stores.py
+++ vector_stores.py
@@ -1,49 +1,55 @@
 from langchain.vectorstores import Chroma, Redis
 
+from embeddings import *
+
 REDIS_URL = "redis://localhost:6379"
 VS_INDEX_NAME = "vectorstore"
 
+emb_type = str(
+    input("choose embedding type (openai/hf): ")
+)  # not sure if this is the best way to do this
+embeddings = get_embeddings(emb_type)
 
-def create_vector_store(text, embeddings, persist_directory, vs_type="redis"):
-    if vs_type == "chroma":
-        print(f"Created Chroma Vectorstore at {persist_directory}")
-        print(f"Creating embeddings. May take some minutes...")
-        db = Chroma.from_documents(
-            documents=text,
-            persist_directory=persist_directory,
-            embedding=embeddings,
-        )
-        db.persist()
-    elif vs_type == "redis":
-        print("Created Redis Vectorstore")
-        print(f"Creating embeddings. May take some minutes...")
-        db = Redis.from_documents(
-            documents=text,
-            redis_url=REDIS_URL,
-            index_name=VS_INDEX_NAME,
-            embedding=embeddings,
-        )
-    else:
-        raise ValueError("Vectorstore type not supported")
+
+def create_chroma_vector_store(text, persist_directory):
+    print(f"Created Chroma Vectorstore at {persist_directory}")
+    print(f"Creating embeddings. May take some minutes...")
+    db = Chroma.from_documents(
+        documents=text,
+        persist_directory=persist_directory,
+        embedding=embeddings,
+    )
+    db.persist()
     return db
 
 
-def load_vector_store(
-    embeddings, persist_directory, index_name=VS_INDEX_NAME, vs_type="redis"
-):
-    if vs_type == "chroma":
-        print(f"Appending to existing Chroma Vectorstore at {persist_directory}")
-        db = Chroma(
-            persist_directory=persist_directory,
-            embedding_function=embeddings,
-        )
-    elif vs_type == "redis":
-        print("Updating existing Redis Vectorstore")
-        db = Redis.from_existing_index(
-            redis_url=REDIS_URL,
-            index_name=index_name,
-            embedding=embeddings,
-        )
-    else:
-        raise ValueError("Vectorstore type not supported")
+def create_redis_vector_store(text, metadata):
+    print("Created Redis Vectorstore")
+    print(f"Creating embeddings. May take some minutes...")
+    db, keys = Redis.from_texts_return_keys(
+        texts=text,
+        metadatas=metadata,
+        redis_url=REDIS_URL,
+        index_name=VS_INDEX_NAME,
+        embedding=embeddings,
+    )
+    return db, keys
+
+
+def load_redis_vector_store(index_name=VS_INDEX_NAME):
+    print("Updating existing Redis Vectorstore")
+    db = Redis.from_existing_index(
+        redis_url=REDIS_URL,
+        index_name=index_name,
+        embedding=embeddings,
+    )
+    return db
+
+
+def load_chroma_vector_store(persist_directory):
+    print(f"Appending to existing Chroma Vectorstore at {persist_directory}")
+    db = Chroma(
+        persist_directory=persist_directory,
+        embedding_function=embeddings,
+    )
     return db
