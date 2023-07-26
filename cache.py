67959ad58b4cc5e36926a01a77d7db43d79a63d0
Sparsh Gupta <gsparsh220598@gmail.com>
2023-07-26 19:56:19 +0530

    scraping cache for now - no support from langchain

--- cache.py
+++ cache.py
@@ -1,4 +1,4 @@
-from langchain.cache import RedisCache, RedisSemanticCache
+from langchain.cache import RedisCache, RedisSemanticCache, SQLiteCache, InMemoryCache
 import langchain
 from embeddings import *
 
@@ -6,10 +6,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-REDIS_URL = "redis://localhost:6379"
+REDIS_URL = "redis://localhost:6360"
 emb_type = os.environ.get("EMB_TYPE")
 
 # TODO: ADD FIX FOR THIS, THIS IS NOT WORKING
 langchain.llm_cache = RedisSemanticCache(
     redis_url="redis://localhost:6379", embedding=get_embeddings(emb_type)
 )
+
+# langchain.llm_cache = SQLiteCache(database_path=".langchain.db")
+
+# langchain.llm_cache = InMemoryCache()
